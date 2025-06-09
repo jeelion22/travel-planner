6c0ca563f62e2688da02b172958805bb4acb6703
jeeva <jeelion22@gmail.com>
2025-06-09 18:52:26 +0530

    update

--- server/controllers/tripController.js
+++ server/controllers/tripController.js
@@ -98,6 +98,8 @@ const tripController = {
     try {
       const userId = req.userId;
 
+      console.log(userId);
+
       const allTrips = await Trip.find({ userId });
 
       if (allTrips.length === 0) {
@@ -106,6 +108,7 @@ const tripController = {
 
       res.status(200).json(allTrips);
     } catch (err) {
+      console.log(err);
       res.status(500).json({ message: err.message });
     }
   },
