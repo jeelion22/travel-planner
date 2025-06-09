35d893247ab97f85c1a9f66661b0d532c64a9f92
jeeva <jeelion22@gmail.com>
2025-06-09 12:13:42 +0530

    update

--- server/app.js
+++ server/app.js
@@ -2,32 +2,19 @@ const express = require("express");
 
 const userRouter = require("./routes/userRoutes");
 const adminRouter = require("./routes/adminRoutes");
-
+const path = require("path");
 const cors = require("cors");
-
 const app = express();
-
 const cookieParser = require("cookie-parser");
-
 const morgan = require("morgan");
 
-
+app.use(express.static(path.__dirname, "../client/dist"));
 
 app.use(
   cors({
-    origin:  ["https://reuniteme.netlify.app",],
-  
+    origin: ["https://reuniteme.netlify.app"],
   })
 );
-
-// app.use((req, res, next) => {
-//   req.header("Access-Control-Allow-Origin", "https://reuniteme.netlify.app");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
 
 app.use(cookieParser());
 
@@ -36,6 +23,10 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "Welcome to the ReUniteME's API end points!" });
